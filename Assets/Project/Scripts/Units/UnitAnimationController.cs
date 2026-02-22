34ff66adf522da68db8f47d0dece0ed775509aa4
Ssste <148126801+Kalorwar@users.noreply.github.com>
2026-02-22 23:43:59 +0300

    Added unitIdle and destroy animations

---
+++ Assets/Project/Scripts/Units/UnitAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.Units
+{
+    public class UnitAnimationController
+    {
+        private static readonly int DestroyAnimationName = Animator.StringToHash("Destroy");
+        private readonly Animator _animator;
+
+        public UnitAnimationController(Animator animator)
+        {
+            _animator = animator;
+        }
+
+        public float DestroyAnimationDuration => 0.8f;
+
+        public void AnimateDestroy()
+        {
+            _animator.SetTrigger(DestroyAnimationName);
+        }
+    }
+}
